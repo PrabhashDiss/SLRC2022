f1c24bade2d1ab5eb554e936f5980afa76a9d2eb
PrabhashDiss <prabhashdissanayake2k@gmail.com>
2023-02-26 20:28:47 +0530

    Create robot.py

---
+++ robot.py
@@ -0,0 +1,23 @@
+from components import *
+
+class Robot:
+    def __init__(self):
+        left_motor = Motor(1,1,1)
+        right_motor = Motor(1,1,1)
+
+        compass = Compass()
+        encoder = Encoder()
+        colour_sensor = ColourSensor()
+
+        front_dist_sensor = DistanceSensor()
+        left_dist_sensor = DistanceSensor()
+        right_dist_sensor = DistanceSensor()
+
+        left_IR1 = IRSensor(1)
+        left_IR2 = IRSensor(1)
+        left_IR3 = IRSensor(1)
+        left_IR4 = IRSensor(1)
+        right_IR1 = IRSensor(1)
+        right_IR2 = IRSensor(1)
+        right_IR3 = IRSensor(1)
+        right_IR4 = IRSensor(1)
