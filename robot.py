54917a4ece0b30f6470034cdd7b0a6e078ac158b
PrabhashDiss <prabhashdissanayake2k@gmail.com>
2023-03-01 19:25:04 +0530

    Update robot.py

--- robot.py
+++ robot.py
@@ -178,6 +178,18 @@ class Robot:
          d | | f
           g _
         '''
+        NumberEdges = {
+            0: {"a": True, "b": True, "c": True, "d": True, "e": False, "f": True, "g": True},
+            1: {"a": False, "b": False, "c": True, "d": False, "e": False, "f": True, "g": False},
+            2: {"a": False, "b": True, "c": True, "d": True, "e": True, "f": False, "g": True},
+            3: {"a": False, "b": True, "c": True, "d": False, "e": True, "f": True, "g": True},
+            4: {"a": True, "b": False, "c": True, "d": False, "e": True, "f": True, "g": False},
+            5: {"a": True, "b": True, "c": False, "d": False, "e": True, "f": True, "g": True},
+            6: {"a": True, "b": True, "c": False, "d": True, "e": True, "f": True, "g": True},
+            7: {"a": False, "b": True, "c": True, "d": False, "e": False, "f": True, "g": False},
+            8: {"a": True, "b": True, "c": True, "d": True, "e": True, "f": True, "g": True},
+            9: {"a": True, "b": True, "c": True, "d": False, "e": True, "f": True, "g": False}
+        }
         NumberPaths = {
             'a': [("turn_right", None), ("move_forward", 70), ("turn_left", None), ("move_forward", 70), ("turn_left", None), ("move_forward", 35)],
             'b': [("turn_right", None), ("move_forward", 35), ("turn_left", None), ("move_forward", 35)],
